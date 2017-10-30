644bdd4d4d68488c318e7d8dbfb23458535f6c6a
Anmol Sandhu <anmolsandhu3096@gmail.com>
2017-10-29 20:20:52 -0500

    Added Food model and its type as dish and ingredient.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030005623) do
+ActiveRecord::Schema.define(version: 20171030011555) do
 
   create_table "drinks", force: :cascade do |t|
     t.string "type"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "dish"
+    t.text "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 100.times do
-  Movie.create(name: Faker::Name.name)
+  Food.create(dish: Faker::Food.dish, ingredients: Faker::Food.ingredient)
 end
